bf28105982ddc3509d5b9593697ceb31cc6371de
dimonProto <dimonvkedax@gmail.com>
2021-12-01 18:05:57 +0200

    ready 1.23

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Provider store={store}>
           <BrowserRouter>
               <Routes>
-                  <Route path="/" element={<CitiesList />}/>
+                  <Route path="/city-weather" element={<CitiesList />}/>
                   <Route path="/city/:id" element={<City />}/>
                   <Route path="*" element={<NotFound />} />
               </Routes>
